f9ce974bdfd18c9d53f90528685db80601c2ef1a
Onyedikachi <onyizboy@gmail.com>
2025-02-13 21:43:40 +1100

    new

--- src/app/services/waitlist.service.ts
+++ src/app/services/waitlist.service.ts
@@ -6,13 +6,12 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class WaitlistService {
-  private apiUrl = 'http://localhost:3000/api';
-  // private apiUrl = 'https://prereetaila.vercel.app/api'; // Use environment variable in production
+  private apiUrl =
+    'https://prereetaila-x70yjm7hf-kaccipluggs-projects.vercel.app/api';
 
   constructor(private http: HttpClient) {}
 
   joinWaitlist(data: any): Observable<any> {
-    console.log('Sending data to server:', data);
     return this.http.post(`${this.apiUrl}/waitlist`, data);
   }
 }
