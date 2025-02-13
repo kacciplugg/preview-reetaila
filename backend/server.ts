f11e41526aca7674cf9a03c0d75b7cf58038809a
Onyedikachi <onyizboy@gmail.com>
2025-02-13 19:34:50 +1100

    server update

--- backend/server.ts
+++ backend/server.ts
@@ -37,9 +37,11 @@ mongoose
   });
 
 // Start server
-const PORT = process.env['PORT'] || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const PORT = Number(process.env['PORT']) || 3000;
+const HOST = process.env['HOST'] || '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 app.use('/api', waitlistRoutes);
